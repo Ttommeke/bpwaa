7ffdafd3bc8d90fc78540efa984549771f8c5f4d
Ttommeke <tomvalkeneers@hotmail.com>
2017-04-02 18:33:47 +0200

    tijdelijk

--- MPEGDASH/SingleFile/StreamsetComponent.js
+++ MPEGDASH/SingleFile/StreamsetComponent.js
@@ -1,10 +1,10 @@
 Vue.component('streamset-comp', {
     "template": `
         <div>
-            <h4>Streamset {{name}} of type: {{streamset.contentType}}</h4> <button @click="download">Play and download</button>
+            <h4>Streamset {{streamset.id}} of type: {{streamset.contentType}}</h4> <button @click="download">Play and download</button>
         </div>
     `,
-    "props": ["streamset", "name"],
+    "props": ["streamset"],
     "data" : function() {
         return {
             buffer : undefined
@@ -12,9 +12,11 @@ Vue.component('streamset-comp', {
     },
     "methods" : {
         "download": function() {
-            Vue.http.get(this.streamset).then(function(resp) {
+            /*Vue.http.get(this.streamset).then(function(resp) {
                 console.log(resp);
-            });
+            });*/
+            console.log(this.streamset);
+            console.log(this.streamset.id);
         },
         "play":  function() {
 

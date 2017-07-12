6c9c340e4ba2961570492aff00f9298a15f3dcd3
Ttommeke <tomvalkeneers@hotmail.com>
2017-07-12 11:46:03 +0200

    fixed port bug

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ var express = require('express');
 var app = express();
 var mime = require('mime');
 
+var port = process.env.PORT || 5000;
+
 mime.define({
     'text/xml': ['mpd'],
 });
 
 app.use('/', express.static(__dirname));
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080. http://localhost:8080');
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '. http://localhost:' + port);
 })
